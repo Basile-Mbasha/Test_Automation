84ee6a7352cd0f73f5ea53baebc11f30eea3532a
basilembasha <mbasiler@outlook.com>
2023-04-04 00:06:32 -0400

    refactored selenium tests

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,26 +1,38 @@
 package base;
 
-import org.openqa.selenium.Dimension;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class BaseTests {
 
     private WebDriver driver;
 
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver"); // add .exe on Windows OS
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver"); // add .exe extension on Windows
         driver = new ChromeDriver();
+
+        // Launch browser and load URL
         driver.get("https://the-internet.herokuapp.com/");
-        driver.manage().window().maximize();
-        driver.manage().window().setSize(new Dimension(375, 812));
-        driver.getTitle();
-        System.out.println(driver.getTitle());
-        driver.close();
+
+        // Get the Shifting Content link and click it
+        driver.findElement(By.linkText("Shifting Content")).click();
+
+        // Get the Example 1: Menu Element and click it
+        driver.findElement(By.linkText("Example 1: Menu Element")).click();
+
+        // Get the list of menu items in Example 1 and print out it's size
+        List<WebElement> menuItems = driver.findElements(By.tagName("li"));
+        System.out.printf("In Example 1, the number of menu items is %d\n", menuItems.size());
+
+        driver.quit();
     }
 
     public static void main(String[] args) {
-        BaseTests test = new BaseTests();
-        test.setUp();
+        BaseTests baseTests = new BaseTests();
+        baseTests.setUp();
     }
 }
